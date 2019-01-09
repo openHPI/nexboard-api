c263b6eff6ba8e9817b4c231b8e45c689b24c44d
Stephan Detje <stephan.detje@nexenio.com>
2019-01-09 13:36:46 +0100

    add pact for create project

--- spec/pact_helper.rb
+++ spec/pact_helper.rb
@@ -4,7 +4,6 @@ require 'nexboard_api'
 
 Pact.service_consumer "OpenHPI" do
   has_pact_with "nexboard" do
-    puts "Starting mock nexboard server"
     mock_service :nexboard do
       port 1234
     end
--- spec/pacts/nexboard_api_pact_spec.rb
+++ spec/pacts/nexboard_api_pact_spec.rb
@@ -5,6 +5,8 @@ describe NexboardApi, :pact => true do
   let(:user_id) { '1337' }
   let(:base_url) { 'http://localhost:1234/' }
   let(:params)  { {api_key: api_key, user_id: user_id, base_url: base_url} }
+  let(:encoded_credentials) { URI::encode('token=' + api_key + '&userId=' + user_id) }
+  let(:encoded_token) { URI::encode('token=' + api_key) }
   client = nil
 
   before do
@@ -16,9 +18,9 @@ describe NexboardApi, :pact => true do
     let(:response) { [{project_id: '1'}, {project_id: '2'}] }
 
     before do
-      nexboard.given("some projects exist").
-          upon_receiving("a request for all project ids").
-          with(method: :get, path: '/projects', query: URI::encode('token=' + api_key + '&userId=' + user_id)).
+      nexboard.given("user has valid credentials and some projects exist").
+          upon_receiving("a request to get all projects").
+          with(method: :get, path: '/projects', query: encoded_credentials).
           will_respond_with(
               status: 200,
               headers: {'Content-Type' => 'application/json'},
@@ -28,6 +30,31 @@ describe NexboardApi, :pact => true do
     it "returns the project ids" do
       project_ids = client.get_project_ids
       expect(project_ids).to eq(['1', '2'])
+    end
+
+  end
+
+  describe "create_project" do
+    let(:title) { 'A new project' }
+    let(:description) { 'This is a new project' }
+    let(:project_id) { '1' }
+    let(:response) { {project_id: project_id, title: title} }
+    let(:project_data) { {title: title, description: description} }
+    let(:post_body) { {title: title, description: description, user_id: user_id} }
+
+    before do
+      nexboard.given("user has valid credentials").
+          upon_receiving("a request to create a project").
+          with(method: :post, path: '/projects', query: encoded_token, body: post_body).
+          will_respond_with(
+              status: 200,
+              headers: {'Content-Type' => 'application/json'},
+              body: response)
+    end
+
+    it "returns a new project" do
+      project = client.create_project(project_data)
+      expect(project.project_id).to eq(project_id)
     end
 
   end
