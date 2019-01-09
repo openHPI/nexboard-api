11ca47a983a4db7b136c30922c64366876407ebe
Stephan Detje <stephan.detje@nexenio.com>
2019-01-09 13:50:08 +0100

    add pact tests for get and create board

--- spec/pacts/nexboard_api_pact_spec.rb
+++ spec/pacts/nexboard_api_pact_spec.rb
@@ -5,14 +5,10 @@ describe NexboardApi, :pact => true do
   let(:user_id) { '1337' }
   let(:base_url) { 'http://localhost:1234/' }
   let(:params)  { {api_key: api_key, user_id: user_id, base_url: base_url} }
-  let(:encoded_credentials) { URI::encode('token=' + api_key + '&userId=' + user_id) }
-  let(:encoded_token) { URI::encode('token=' + api_key) }
-  client = nil
+  let(:encoded_credentials) { URI::encode("token=#{api_key}&userId=#{user_id}") }
+  let(:encoded_token) { URI::encode("token=#{api_key}") }
+  let(:client) { NexboardApi.new **params }
 
-  before do
-    # Configure your client to point to the stub service on localhost using the port you have specified
-    client = NexboardApi.new **params
-  end
 
   describe "get_project_ids" do
     let(:response) { [{project_id: '1'}, {project_id: '2'}] }
@@ -55,6 +51,55 @@ describe NexboardApi, :pact => true do
     it "returns a new project" do
       project = client.create_project(project_data)
       expect(project.project_id).to eq(project_id)
+    end
+
+  end
+
+  describe "get_boards_for_project" do
+    let(:title) { 'A board' }
+    let(:project_id) { '1' }
+    let(:board_id) { '101' }
+    let(:response) { [{boardId: board_id, title: title}] }
+
+    before do
+      nexboard.given("user has valid credentials and an existing project with boards").
+          upon_receiving("a request to get boards of a project").
+          with(method: :get, path: "/projects/#{project_id}/boards", query: encoded_credentials).
+          will_respond_with(
+              status: 200,
+              headers: {'Content-Type' => 'application/json'},
+              body: response)
+    end
+
+    it "returns a new project" do
+      boards = client.get_boards_for_project( {project_id: project_id} )
+      expect(boards.first.boardId).to eq(board_id)
+    end
+
+  end
+
+  describe "create_board_for_project" do
+    let(:title) { 'A new project' }
+    let(:description) { 'This is a new project' }
+    let(:project_id) { '1' }
+    let(:board_id) { '101' }
+    let(:response) { {board_id: board_id, project_id: project_id, title: title} }
+    let(:board_data) { {title: title, description: description, project_id: project_id} }
+    let(:post_body) { {title: title, description: description, project_id: project_id, user_id: user_id} }
+
+    before do
+      nexboard.given("user has valid credentials and an existing project").
+          upon_receiving("a request to create a project").
+          with(method: :post, path: '/boards', query: encoded_token, body: post_body).
+          will_respond_with(
+              status: 200,
+              headers: {'Content-Type' => 'application/json'},
+              body: response)
+    end
+
+    it "returns a new project" do
+      board = client.create_board_for_project(board_data)
+      expect(board.board_id).to eq(board_id)
     end
 
   end
